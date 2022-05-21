84ab806282633ff96533b1bd483918e6ad86ba92
tuhuynh <tuhuynh1902@gmail.com>
2022-05-21 16:19:44 +0700

    #37

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,6 +20,26 @@ let handleLogin = async (req, res) => {
   });
 };
 
+let handleGetAllUser = async (req, res) => {
+  let id = req.body.id; //ALL or Id
+
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: 'Missing required parameters',
+      users: [],
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: 'ok',
+    users,
+  });
+};
+
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUser: handleGetAllUser,
 };
